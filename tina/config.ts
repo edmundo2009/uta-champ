1df2c816d41c10c442cf40bb6e14913e17b918ba
edmundo2009 <daimakaveli@gmail.com>
2024-11-07 03:29:04 +0900

    created tina-webhooks

--- tina/config.ts
+++ tina/config.ts
@@ -1,80 +1,71 @@
-import { defineConfig, defineSchema } from "tinacms";
+import { defineConfig } from "tinacms";
 
-const schema = defineSchema({
-  collections: [
-    {
-      name: "faq",
-      label: "質問回答を追加",
-      path: "content/faqs",
-      format: "md",
-      ui: {
-        router: ({ document }) => "/faqs",
-      },
-
-      fields: [
-        {
-          type: "object",
-          name: "faqs",
-          label: "FAQ Items",
-          list: true,
-          ui: {
-            itemProps: (item) => ({
-              label: item.question,
-            }),
-          },
-          fields: [
-            {
-              type: "string",
-              name: "question",
-              label: "質問",
-              required: true,
-            },
-            {
-              type: "string",
-              name: "answer",
-              label: "回答",
-              required: true,
-              ui: {
-                component: "textarea",
-              },
-            },
-            {
-              type: "string",
-              name: "category",
-              label: "カテゴリー",
-              required: false,
-              options: ["本番", "エントリー", "審査", "表彰", "支払い", "その他"],
-            },
-          ],
-        },
-      ],
-    },
-  ],
-});
+const branch = process.env.NEXT_PUBLIC_TINA_BRANCH ;
+const clientId = process.env.NEXT_PUBLIC_TINA_CLIENT_ID ;
+const token = process.env.TINA_TOKEN ;
 
 export default defineConfig({
-  schema,
-  branch: process.env.NEXT_PUBLIC_TINA_BRANCH,
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  token: process.env.TINA_TOKEN,
-  // branch: process.env.NEXT_PUBLIC_TINA_BRANCH || "master",
-  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "01248537-bed9-4526-8eb2-e7b38f3ad462",
-  // token: process.env.TINA_TOKEN || "f2dd49bd4cd2dc70eb0ef12feadc783dc472df72",
+  branch,
+  clientId,
+  token,
   build: {
+    outputFolder: "admin",
     publicFolder: "public",
-    outputFolder: "admin"
   },
   media: {
     tina: {
+      mediaRoot: "uploads",
       publicFolder: "public",
-      mediaRoot: "uploads"
-    }
+    },
   },
-  // Add this section
-  webhook: {
-    url: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/revalidate`
-      : 'http://localhost:3000/api/revalidate',
-    mode: 'data'
-  }
+  schema: {
+    collections: [
+      {
+        name: "faq",
+        label: "質問回答を追加",
+        path: "content/faqs",
+        format: "md",
+        ui: {
+          router: ({ document }) => "/faqs",
+        },
+        fields: [
+          {
+            type: "object",
+            name: "faqs",
+            label: "FAQ Items",
+            list: true,
+            ui: {
+              itemProps: (item) => ({
+                label: item.question,
+              }),
+            },
+            fields: [
+              {
+                type: "string",
+                name: "question",
+                label: "質問",
+                required: true,
+              },
+              {
+                type: "string",
+                name: "answer",
+                label: "回答",
+                required: true,
+                ui: {
+                  component: "textarea",
+                },
+              },
+              {
+                type: "string",
+                name: "category",
+                label: "カテゴリー",
+                required: false,
+                options: ["本番", "エントリー", "審査", "表彰", "支払い", "その他"],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 });
