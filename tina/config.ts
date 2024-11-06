3ea4c6cc97f1c6b95c142d3a41a862ec24b1f2a2
edmundo2009 <daimakaveli@gmail.com>
2024-11-07 02:59:30 +0900

    updated tinacms to 2.4

--- tina/config.ts
+++ tina/config.ts
@@ -10,36 +10,7 @@ const schema = defineSchema({
       ui: {
         router: ({ document }) => "/faqs",
       },
-      // fields: [
-      //   {
-      //     type: "object",
-      //     name: "faqs",
-      //     label: "FAQ Items",
-      //     list: true,
-      //     ui: {
-      //       itemProps: (item) => ({
-      //         label: item.question,
-      //       }),
-      //     },
-      //     fields: [
-      //       {
-      //         type: "string",
-      //         name: "question",
-      //         label: "Question",
-      //         required: true,
-      //       },
-      //       {
-      //         type: "string",
-      //         name: "answer",
-      //         label: "Answer",
-      //         required: true,
-      //         ui: {
-      //           component: "textarea",
-      //         },
-      //       },
-      //     ],
-      //   },
-      // ],
+
       fields: [
         {
           type: "object",
@@ -83,9 +54,12 @@ const schema = defineSchema({
 
 export default defineConfig({
   schema,
-  branch: process.env.NEXT_PUBLIC_TINA_BRANCH || "master",
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "01248537-bed9-4526-8eb2-e7b38f3ad462",
-  token: process.env.TINA_TOKEN || "f2dd49bd4cd2dc70eb0ef12feadc783dc472df72",
+  branch: process.env.NEXT_PUBLIC_TINA_BRANCH,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  // branch: process.env.NEXT_PUBLIC_TINA_BRANCH || "master",
+  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "01248537-bed9-4526-8eb2-e7b38f3ad462",
+  // token: process.env.TINA_TOKEN || "f2dd49bd4cd2dc70eb0ef12feadc783dc472df72",
   build: {
     publicFolder: "public",
     outputFolder: "admin"
